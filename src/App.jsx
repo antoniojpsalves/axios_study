4024ac2a6bb6e5042089d85a5fc6171c494ecfa6
Antonio <antonio.jpsalves@gmail.com>
2023-01-09 21:24:39 -0300

    Criando e utilizando como default uma instância do axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+import axiosInstance from './helpers/axios-instance'
 
 import './App.css'
 
 function App() {
 
   const [pokemonsList, setPokemonsList] = useState([]);
+  const [pokedexList, setPokedexList] = useState([]);
 
   useEffect(() => {
     
-    axios.get('https://pokeapi.co/api/v2/pokemon')
+    axiosInstance.get('pokemon')
     .then((res) => {
       console.log(res)
       setPokemonsList(res.data.results)
+    })
+    .catch(error => console.error(error))
+    .finally();
+
+
+    axiosInstance.get('pokedex')
+    .then((res) => {
+      console.log(res)
+      setPokedexList(res.data.results)
     })
     .catch(error => console.error(error))
     .finally();
@@ -23,6 +33,7 @@ function App() {
   return (
     <div className="App">
       {pokemonsList.map((pokemon, index) => <div key={index}>{pokemon.name}</div>)}
+      {pokedexList.map((region, index) => <div key={index}>{region.name}</div>)}
     </div>
   )
 }
